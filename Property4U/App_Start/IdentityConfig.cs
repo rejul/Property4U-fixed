df2470044605207da100163692c9a2864e09b12b
rejul <rejul.reghu@gmail.com>
2020-07-24 22:33:04 +0530

    minor fix
    removed the dark horse .bak (I think developer wrote that script during sleepy time, still I am new to whole fucking ASP .NET , Need some job man..my sleepness nights are on my way)

--- Property4U/App_Start/IdentityConfig.cs
+++ Property4U/App_Start/IdentityConfig.cs
@@ -113,40 +113,40 @@ namespace IdentitySample.Models
 
         //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
         public static void InitializeIdentityForEF(ApplicationDbContext db) {
-            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
-            const string name = "admin@p4u.com.pk";
-            const string password = "Admin@123456";
-            const string roleName = "Admin";
+        //    var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+        //    var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+        //    const string name = "admin@p4u.com.pk";
+        //    const string password = "Admin@123456";
+        //    const string roleName = "Admin";
              
-            /* Extending User Attributes for Property4U */
-            const string firstName = "Ahmed";
-            const string lastName = "Osman";
-            const string address = "Lahore, Pakistan";
-            const string city = "Lahore";
-            const string state = "Punjab";
-            const string postalCode = "54000";
+        //    /* Extending User Attributes for Property4U */
+        //    const string firstName = "Ahmed";
+        //    const string lastName = "Osman";
+        //    const string address = "Lahore, Pakistan";
+        //    const string city = "Lahore";
+        //    const string state = "Punjab";
+        //    const string postalCode = "54000";
 
-            //Create Role Admin if it does not exist
-            var role = roleManager.FindByName(roleName);
-            if (role == null) {
-                // Modified ApplicationRoleManager Class "IdentityRole" with our Custom "ApplicationRole" - P4U
-                role = new ApplicationRole(roleName);
-                var roleresult = roleManager.Create(role);
-            }
+        //    //Create Role Admin if it does not exist
+        //    var role = roleManager.FindByName(roleName);
+        //    if (role == null) {
+        //        // Modified ApplicationRoleManager Class "IdentityRole" with our Custom "ApplicationRole" - P4U
+        //        role = new ApplicationRole(roleName);
+        //        var roleresult = roleManager.Create(role);
+        //    }
 
-            var user = userManager.FindByName(name);
-            if (user == null) {
-                user = new ApplicationUser { UserName = name, Email = name, FirstName = firstName, LastName = lastName, Address = address, City = city, State = state, PostalCode = postalCode };
-                var result = userManager.Create(user, password);
-                result = userManager.SetLockoutEnabled(user.Id, false);
-            }
+        //    var user = userManager.FindByName(name);
+        //    if (user == null) {
+        //        user = new ApplicationUser { UserName = name, Email = name, FirstName = firstName, LastName = lastName, Address = address, City = city, State = state, PostalCode = postalCode };
+        //        var result = userManager.Create(user, password);
+        //        result = userManager.SetLockoutEnabled(user.Id, false);
+        //    }
 
-            // Add user admin to Role Admin if not already added
-            var rolesForUser = userManager.GetRoles(user.Id);
-            if (!rolesForUser.Contains(role.Name)) {
-                var result = userManager.AddToRole(user.Id, role.Name);
-            }
+        //    // Add user admin to Role Admin if not already added
+        //    var rolesForUser = userManager.GetRoles(user.Id);
+        //    if (!rolesForUser.Contains(role.Name)) {
+        //        var result = userManager.AddToRole(user.Id, role.Name);
+        //    }
         }
     }
 
