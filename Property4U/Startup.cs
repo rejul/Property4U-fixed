2a8ed3f493929d87dfa16f3fdfcbde68f3707738
rejul <rejul.reghu@gmail.com>
2020-07-24 18:05:47 +0530

    Updates
    working on Visual studio 2019

--- Property4U/Startup.cs
+++ Property4U/Startup.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+[assembly: OwinStartup(typeof(IdentitySample.Startup))]
 
 namespace IdentitySample
 {
-    [assembly: OwinStartup(typeof(IdentitySample.Startup))]
+   
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
